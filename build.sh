16a8b1ba7a5d1386736938482103e5f815bed968
wumo1016 <760478279@qq.com>
2021-07-21 17:56:43 +0800

    chore

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# commit提交所有文件
+git add -A
+git commit -m 'chore'
+
+# 推送到github
+git push -f https://gitee.com/wumo1016/wmview-docs.git master
+
+cd -
